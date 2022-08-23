e080365b023736a2108e11c59875aeb867a2df0b
Arbaz86 <arbaz10042002@gmail.com>
2022-08-23 18:04:26 +0530

    ADDED PRODUCTS DATA

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./Homepage";
+import SearchProduct from "./SearchProduct";
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/search" element={<SearchProduct />} />
     </Routes>
   );
 };
---
+++ src/Pages/SearchProduct.jsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const SearchProduct = () => {
+  return <Box></Box>;
+};
+
+export default SearchProduct;
