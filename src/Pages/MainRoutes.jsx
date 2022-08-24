de4bbb6309f4d7528e9cabdfeb78a06f9995bf06
vaibhav jadhav <95289821+va1bhav22@users.noreply.github.com>
2022-08-24 22:52:57 +0530

    day2issuecomplete

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./Homepage";
+import Login from "./Login";
 import SearchProduct from "./SearchProduct";
+import SignUp from "./SignUp";
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/search" element={<SearchProduct />} />
+      <Route path='/signup' element={<SignUp/>}/>
+        <Route path='login' element={<Login/>}/>
     </Routes>
   );
 };
