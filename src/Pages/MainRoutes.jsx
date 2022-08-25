914171155f1052994c3c9f80d9916309e89dd44a
AnwardeenM <kalilanwar597@gmail.com>
2022-08-25 18:27:09 +0530

    The Navbar work

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
 import Homepage from "./Homepage";
 
 const MainRoutes = () => {
   return (
+    <>
+    <Navbar />
     <Routes>
       <Route path="/" element={<Homepage />} />
     </Routes>
+    </>
   );
 };
 
--- src/Redux/AuthReudcer/reducer.js
+++ src/Redux/AuthReudcer/reducer.js
@@ -1,12 +1,12 @@
-import * as types from "./actionTypes";
+// import * as types from "./actionTypes";
 
 const initialState = {};
 
-export const reducer = (state = initialState, action) => {
-  const { type, payload } = action;
+export const reducer = (state = initialState) => {
+  // const { type, payload } = action;
 
-  switch (type) {
-    default:
+  // switch (type) {
+    // default:
       return state;
-  }
+  // }
 };
