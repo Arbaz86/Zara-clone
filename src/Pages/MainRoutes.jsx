58e0ebd1f9a3e2e9bac6c25f56a919a5b43f39be
vaibhav jadhav <95289821+va1bhav22@users.noreply.github.com>
2022-08-26 23:41:57 +0530

    mainfooter

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Footer from "../Components/Footer";
 import Homepage from "./Homepage";
 import Login from "./Login";
 import SearchProduct from "./SearchProduct";
@@ -8,10 +9,11 @@ import SignUp from "./SignUp";
 const MainRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
+      <Route path="/homepage" element={<Homepage />} />
       <Route path="/search" element={<SearchProduct />} />
       <Route path='/signup' element={<SignUp/>}/>
         <Route path='login' element={<Login/>}/>
+        <Route path='/' element={<Footer/>}/>
     </Routes>
   );
 };
