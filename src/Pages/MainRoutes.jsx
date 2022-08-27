caa5770684657896b34e633735fed1ba55effa27
vaibhav jadhav <95289821+va1bhav22@users.noreply.github.com>
2022-08-27 16:52:32 +0530

    landingpage

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Footer from "../Components/Footer";
 import Homepage from "./Homepage";
 import Login from "./Login";
+import MenLandingPage from "./MenLandingPage";
 import SearchProduct from "./SearchProduct";
 import SignUp from "./SignUp";
 
@@ -13,7 +14,8 @@ const MainRoutes = () => {
       <Route path="/search" element={<SearchProduct />} />
       <Route path='/signup' element={<SignUp/>}/>
         <Route path='login' element={<Login/>}/>
-        <Route path='/' element={<Footer/>}/>
+        <Route path='/footer' element={<Footer/>}/>
+        <Route path='/' element={<MenLandingPage/>}/>
     </Routes>
   );
 };
